07f1e7395fb6cefbc0acbf5b7f02cb05b00aba09
nichlas0010 <nichlas0010@gmail.com>
2021-12-08 23:52:47 +0000

    fuadce

--- src/main/java/rocks/ashleigh/lovejoy/LoveJoyController.java
+++ src/main/java/rocks/ashleigh/lovejoy/LoveJoyController.java
@@ -40,7 +40,7 @@ public class LoveJoyController {
         prop.put("mail.smtp.auth", true);
         prop.put("mail.smtp.starttls.enable", "true");
         prop.put("mail.smtp.host", "smtp.gmail.com");
-        prop.put("mail.smtp.port", "465");
+        prop.put("mail.smtp.port", "587");
         prop.put("mail.smtp.ssl.trust", "smtp.gmail.com");
         this.emailUser = emailUser;
 
